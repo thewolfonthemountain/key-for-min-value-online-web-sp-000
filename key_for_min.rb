8b8cf1e6bfb12d3147a1bcb71bcfa42323ff85c5
Sean Langton <slangton1@gmail.com>
2018-10-30 17:58:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = integer.new
   min_key = ""
   name_hash.each do |key, value|
-    if value < min_value || min_value = 0 then
+    if value < min_value then
       min_value = value
       min_key = key
     end
