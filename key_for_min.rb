a2f1e55b1c5ece2c727de02f428e109c7f7c9428
Sean Langton <slangton1@gmail.com>
2018-10-30 18:02:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash = {} then return nil
+  if name_hash == {} then return nil
   else
   min_value = 1000
   min_key = ""
